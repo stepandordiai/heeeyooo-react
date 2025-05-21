b2f7fdef09ae087bdd8ae9a6e67aafea2c7bd522
Stepan Dordiai <stepandordiai@gmail.com>
2025-05-21 09:41:26 +0200

    v1.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import "./App.scss";
 import Header from "./Components/Header/Header";
+import workData from "./data/work-data.json";
+import "./App.scss";
 
 function App() {
 	return (
 		<>
 			<Header />
+			<p className="work__title">Work</p>
+			<div className="work">
+				{workData.map((project) => {
+					return (
+						<a
+							key={project.id}
+							className="work__project-card-link"
+							href={project.siteUrl}
+							target="_blank"
+						>
+							<img src={project.img} alt="" />
+							<p className="work__project-card-name">{project.name}</p>
+						</a>
+					);
+				})}
+			</div>
 		</>
 	);
 }
