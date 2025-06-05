2f46160eae45a4fbc6fd0b13af24a8ed0eb33ecf
Stepan Dordiai <stepandordiai@gmail.com>
2025-06-06 00:16:10 +0200

    Update Portfolio

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -2,8 +2,15 @@ import { Helmet } from "react-helmet";
 import workData from "../../data/work-data.json";
 import arrowIcon from "/icons/arrow-upper-right.png";
 import "./Portfolio.scss";
+import { useEffect, useState } from "react";
 
 const Portfolio = () => {
+	const [layout, setLayout] = useState("works__list");
+
+	function handleLayout(props) {
+		setLayout(props);
+	}
+
 	return (
 		<>
 			<Helmet>
@@ -11,21 +18,45 @@ const Portfolio = () => {
 				<link rel="canonical" href="https://heeeyooo.studio/portfolio" />
 			</Helmet>
 			<h1>All works</h1>
-			<div className="works">
-				{workData.map((project) => {
-					return (
-						<a
-							key={project.id}
-							className="work"
-							href={project.siteUrl}
-							target="_blank"
-						>
-							<p className="work__name">{project.name}</p>
-							<img width={20} height={20} src={arrowIcon} alt="" />
-						</a>
-					);
-				})}
-			</div>
+			<button onClick={() => handleLayout("works__list")}>List</button>
+			<button onClick={() => handleLayout("works__grid")}>Grid</button>
+			{layout === "works__list" && (
+				<div className={layout}>
+					{workData.map((project) => {
+						return (
+							<a
+								key={project.id}
+								className="work"
+								href={project.siteUrl}
+								target="_blank"
+							>
+								<p className="work__name">{project.name}</p>
+								<img width={20} height={20} src={arrowIcon} alt="" />
+							</a>
+						);
+					})}
+				</div>
+			)}
+			{layout === "works__grid" && (
+				<div className={layout}>
+					{workData.map((project) => {
+						return (
+							<a
+								key={project.id}
+								className="work__grid"
+								href={project.siteUrl}
+								target="_blank"
+							>
+								<img src={project.img} alt="" />
+								<div className="work__details-grid">
+									<p className="work__name">{project.name}</p>
+									<img width={20} height={20} src={arrowIcon} alt="" />
+								</div>
+							</a>
+						);
+					})}
+				</div>
+			)}
 		</>
 	);
 };
