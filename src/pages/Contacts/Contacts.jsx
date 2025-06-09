f71bcaf72e73d0174abea17a96ab19427345e369
Stepan Dordiai <stepandordiai@gmail.com>
2025-06-09 16:35:14 +0200

    Update Contacts and styles

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -8,11 +8,21 @@ const Contacts = () => {
 				<title>Contacts | heeeyooo studio</title>
 				<link rel="canonical" href="https://heeeyooo.studio/contacts" />
 			</Helmet>
-			<h1>Contact us</h1>
-			<div className="contacts__details">
-				<p className="contacts__details-title">Say hello</p>
-				{/* <a href="tel:+420722001016">+420 722 001 016</a> */}
-				<a href="mailto:hello@heeeyooo.studio">hello@heeeyooo.studio</a>
+			<div className="contacts">
+				<div className="contacts__details">
+					<h1>Contact us</h1>
+					<p className="contacts__details-title">Say hello</p>
+					{/* <a href="tel:+420722001016">+420 722 001 016</a> */}
+					<a href="mailto:hello@heeeyooo.studio">hello@heeeyooo.studio</a>
+				</div>
+				<form className="contacts__form" action="">
+					<input type="text" name="firstName" placeholder="First name" />
+					<input type="text" name="email" placeholder="E-mail" />
+					<input type="text" name="tel" placeholder="Phone number" />
+					<button className="contacts__form-btn" type="submit">
+						Submit
+					</button>
+				</form>
 			</div>
 		</>
 	);
