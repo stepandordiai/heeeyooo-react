9865bbbd7c38a72dedf8de01fcf6b5752fb802a2
Stepan Dordiai <stepandordiai@gmail.com>
2025-07-04 16:00:50 +0200

    Update Contacts

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,8 +1,23 @@
 import { Helmet } from "react-helmet";
 import "./Contacts.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Contacts = () => {
+	const [formData, setFormData] = useState({
+		firstName: "",
+		email: "",
+		message: "",
+		howCanWeHelp: "",
+	});
+
+	const handleFormChange = (e) => {
+		const { name, value } = e.target;
+
+		setFormData((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+	};
 	async function initMap() {
 		const centerPosition = { lat: 50.02718425360769, lng: 15.202752716064479 }; // Center of Czech Republic
 
@@ -102,21 +117,93 @@ const Contacts = () => {
 						</p>
 						<p>Got ideas? We've got the skills. Let's team up.</p>
 						<p>Tell us more about yourself and what you've got in mind.</p>
-						<form className="form" action="">
-							<input type="text" placeholder="Your name" />
-							<input type="text" placeholder="you@company.com" />
+						<form
+							className="form"
+							action="https://formspree.io/f/xovddpld"
+							method="POST"
+							// encType="plain/text"
+						>
+							<input
+								onChange={handleFormChange}
+								type="text"
+								name="firstName"
+								placeholder="Your name"
+								value={formData.firstName}
+							/>
+							<input
+								onChange={handleFormChange}
+								type="text"
+								name="email"
+								placeholder="you@company.com"
+								value={formData.email}
+							/>
 							<div className="input-container">
 								<label htmlFor="">Tell us a little about the project...</label>
 								<textarea name="" id=""></textarea>
 							</div>
 							<p>How can we help?</p>
 							<div className="check-container">
-								<input type="checkbox" name="" id="" />
-								<input type="checkbox" name="" id="" />
-								<input type="checkbox" name="" id="" />
-								<input type="checkbox" name="" id="" />
-								<input type="checkbox" name="" id="" />
-								<input type="checkbox" name="" id="" />
+								<div>
+									<label htmlFor="strategy">Strategy</label>
+									<input
+										type="checkbox"
+										name="howCanWeHelp"
+										id="strategy"
+										value="Strategy"
+									/>
+								</div>
+								<div>
+									<label htmlFor="design">Design</label>
+									<input
+										onChange={handleFormChange}
+										type="checkbox"
+										name="howCanWeHelp"
+										id="design"
+										value="Design"
+									/>
+								</div>
+								<div>
+									<label htmlFor="development">Development</label>
+									<input
+										onChange={handleFormChange}
+										type="checkbox"
+										name="howCanWeHelp"
+										id="development"
+										value="Development"
+									/>
+								</div>
+								<div>
+									<label htmlFor="hosting">Hosting</label>
+									<input
+										onChange={handleFormChange}
+										type="checkbox"
+										name="howCanWeHelp"
+										id="hosting"
+										value="Hosting"
+									/>
+								</div>
+								<div>
+									<label htmlFor="website-maintenance">
+										Website Maintenance
+									</label>
+									<input
+										onChange={handleFormChange}
+										type="checkbox"
+										name="howCanWeHelp"
+										id="website-maintenance"
+										value="Website Maintenance"
+									/>
+								</div>
+								<div>
+									<label htmlFor="seo">SEO</label>
+									<input
+										onChange={handleFormChange}
+										type="checkbox"
+										name="howCanWeHelp"
+										id="seo"
+										value="SEO"
+									/>
+								</div>
 							</div>
 							<button type="submit">Let's get started!</button>
 						</form>
